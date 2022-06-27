d2f119ac6bc87fd0e54e54799c1f9a3b46213291
张营 <1511455676@qq.com>
2022-06-27 23:54:46 +0800

    init

--- src/components/x6/index.vue
+++ src/components/x6/index.vue
@@ -7,7 +7,8 @@
 
 <script>
 import { Graph, Addon } from "@antv/x6";
-import { shapeList } from "./shape";
+import { shapeGroups } from "./shape";
+import _ from "lodash-es";
 
 export default {
   name: "x6",
@@ -41,23 +42,6 @@ export default {
       },
     });
 
-
-    // TODO 根据模板组件来自动生成分组
-    // 定义组件面板分组
-    const stencilGroups = [{
-      name: "group1",
-      title: "Group1",
-      collapsable: false,
-      layoutOptions: {
-        columns: 1,
-        dx: 250 / 4, // columns 为1时偏移量（宽度/4）居中
-        rowHeight: 30, // 每行的行高
-        dy: 15 // y轴偏移量（边距）
-      }
-    }, {
-      name: "group2",
-      title: "Group(Collapsable)",
-    }]
     // 初始化组件面板
     this.stencil = new Addon.Stencil({
       title: "组件",
@@ -70,28 +54,33 @@ export default {
       collapsable: true,
       stencilGraphWidth: 250, // 模板画布宽度
       // stencilGraphHeight: 140,
-      groups: stencilGroups,
+      groups: _.cloneDeep(shapeGroups).map((item) => {
+        delete item.nodes;
+        return item;
+      }),
     });
     this.$refs["stencilContainer"].appendChild(this.stencil.container);
 
     // 注册组件
-    shapeList.forEach((item) => {
-      Graph.registerVueComponent(item.name, item.node, true);
-    });
-    stencilGroups.forEach((group) => {
-      const filterShapes = shapeList.filter(x => x.group == group.name)
+    shapeGroups.forEach((group) => {
+      group.nodes.forEach((item) => {
+        Graph.registerVueComponent(item.name, item.node, true);
+      });
       this.stencil.load(
-        filterShapes.map((shape) => ({
+        group.nodes.map((node) => ({
           shape: "vue-shape",
-          component: shape.name,
-          width: shape.width || 100, // 模板组件宽度
-          height: shape.height || 24 // 模板组件高度
+          component: node.name,
+          width: node.width || 100, // 模板组件宽度
+          height: node.height || 24, // 模板组件高度
         })),
         group.name
       );
-      this.stencil.resizeGroup(group.name, { height: filterShapes.length * group?.layoutOptions?.rowHeight + group?.layoutOptions?.dy * 2 })
-    })
-
+      this.stencil.resizeGroup(group.name, {
+        height:
+          group.nodes.length * group?.layoutOptions?.rowHeight +
+          group?.layoutOptions?.dy * 2,
+      });
+    });
   },
 };
 </script>
--- src/components/x6/shape/index.js
+++ src/components/x6/shape/index.js
@@ -2,49 +2,74 @@ import "@antv/x6-vue-shape";
 
 import Test from "./Test.vue";
 
-const shapeList = [
+const shapeGroups = [
   {
-    name: "dag-test",
-    group: "group1",
-    node: {
-      template: `<Test />`,
-      components: {
-        Test,
-      },
+    name: "group1",
+    title: "Group1",
+    collapsable: false,
+    layoutOptions: {
+      columns: 1,
+      dx: 250 / 4, // columns 为1时偏移量（宽度/4）居中
+      rowHeight: 30, // 每行的行高
+      dy: 15, // y轴偏移量（边距）
     },
-    width: 100,
-    height: 24
+    nodes: [
+      {
+        name: "dag-test",
+        group: "group1",
+        node: {
+          template: `<Test />`,
+          components: {
+            Test,
+          },
+        },
+        width: 100,
+        height: 24,
+      },
+      {
+        name: "dag-test1",
+        group: "group1",
+        node: {
+          template: `<Test />`,
+          components: {
+            Test,
+          },
+        },
+      },
+      {
+        name: "dag-test2",
+        group: "group1",
+        node: {
+          template: `<Test />`,
+          components: {
+            Test,
+          },
+        },
+      },
+    ],
   },
   {
-    name: "dag-test1",
-    group: "group1",
-    node: {
-      template: `<Test />`,
-      components: {
-        Test,
-      },
+    name: "group2",
+    title: "Group(Collapsable)",
+    layoutOptions: {
+      columns: 1,
+      dx: 250 / 4, // columns 为1时偏移量（宽度/4）居中
+      rowHeight: 30, // 每行的行高
+      dy: 15, // y轴偏移量（边距）
     },
-  },
-  {
-    name: "dag-test2",
-    group: "group1",
-    node: {
-      template: `<Test />`,
-      components: {
-        Test,
+    nodes: [
+      {
+        name: "dag-test3",
+        group: "group1",
+        node: {
+          template: `<Test />`,
+          components: {
+            Test,
+          },
+        },
       },
-    },
-  },
-  {
-    name: "dag-test3",
-    group: "group1",
-    node: {
-      template: `<Test />`,
-      components: {
-        Test,
-      },
-    },
+    ],
   },
 ];
 
-export { shapeList };
+export { shapeGroups };
